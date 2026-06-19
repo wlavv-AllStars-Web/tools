7e358d98124341e182c75ce30c78f5e4e25d6827
wlavv-AllStars-Web <139965773+wlavv-AllStars-Web@users.noreply.github.com>
2026-06-19 11:44:06 +0100

    update image data

--- app/Models/prestashop/AsdImage.php
+++ app/Models/prestashop/AsdImage.php
@@ -404,8 +404,9 @@ class AsdImage extends PrestashopModel
 
         foreach (['thumb', '600'] as $size) {
             foreach ($filenames as $filename) {
-                echo $path = env('RESOURCES_PRODUCTION') . '/asd/brands/' . $idManufacturer . '/' . $size . '/' . $filename . '.webp';
-
+                echo '<br>: ' . $path = env('RESOURCES_PRODUCTION') . '/asd/brands/' . $idManufacturer . '/' . $size . '/' . $filename . '.webp';
+                echo '<br>: ' . file_exists($path);
+                
                 if (file_exists($path)) {
                     dd(4);
                     return $path;

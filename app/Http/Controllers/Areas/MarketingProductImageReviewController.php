1728552eafc2d38508e2a8dd12ecd0f150668b5e
Bruno Fernandes <brunofernandes@all-stars-distribution.com>
2026-07-16 21:14:56 +0100

    fix(marketing): improve product image review gallery

--- app/Http/Controllers/Areas/MarketingProductImageReviewController.php
+++ app/Http/Controllers/Areas/MarketingProductImageReviewController.php
@@ -101,7 +101,7 @@ class MarketingProductImageReviewController extends Controller
                 'id_image' => (int) $image->id_image,
                 'position' => (int) $image->position,
                 'cover' => (bool) $image->cover,
-                'thumbnail_url' => $this->imageUrl((int) $image->id_image, 'small_default'),
+                'thumbnail_url' => $this->imageUrl((int) $image->id_image, 'medium_default'),
                 'large_url' => $this->imageUrl((int) $image->id_image, 'large_default'),
             ]));
     }

c00ca6912c08deae0bd8b4377218b16339b436b2
Bruno Fernandes <brunofernandes@all-stars-distribution.com>
2026-07-30 13:23:44 +0100

    Exclude ASD company from DPD CSV recipient

--- app/Http/Controllers/CustomTools/dpdController.php
+++ app/Http/Controllers/CustomTools/dpdController.php
@@ -46,7 +46,9 @@ class dpdController extends Controller
 
             $data[] = $dpdpais;
             $data[] = $order->customer->id_customer;
-            $data[] = self::dpdText(self::recipientName($order->delivery));
+            $includeCompany = (int) $order->id_shop !== 3;
+
+            $data[] = self::dpdText(self::recipientName($order->delivery, $includeCompany));
             $data[] = self::dpdText($order->delivery->address1 . ' ' . $order->delivery->address2);
             $data[] = $order->delivery->postcode;
             $data[] = self::dpdText($order->delivery->city);
@@ -63,7 +65,7 @@ class dpdController extends Controller
             
             if( ($order->recyclable == 1 ) ){
 
-                $data[] = self::dpdText(self::recipientName($order->invoice));
+                $data[] = self::dpdText(self::recipientName($order->invoice, $includeCompany));
                 $data[] = "ZONA INDUSTRIAL DE GANDRA,LOTE 6";
                 $data[] = "4930-311";
                 $data[] = "VALENCA";
@@ -89,13 +91,13 @@ class dpdController extends Controller
         return Response::download($path.$file, $file, $headers);
     }
 
-    private static function recipientName($address): string
+    private static function recipientName($address, bool $includeCompany = true): string
     {
         $company = trim((string) ($address->company ?? ''));
         $company = trim($company, " \t\n\r\0\x0B|-/");
         $name = trim((string) ($address->firstname ?? '') . ' ' . (string) ($address->lastname ?? ''));
 
-        return $company !== '' ? trim($name . ' (' . $company . ')') : $name;
+        return $includeCompany && $company !== '' ? trim($name . ' (' . $company . ')') : $name;
     }
 
     private static function dpdText($value): string

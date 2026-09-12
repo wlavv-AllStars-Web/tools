6bdbd7653ec7e6683b784f5dd61a2b7212d7ce4c
Bruno Fernandes <brunofernandes@all-stars-distribution.com>
2026-09-12 11:06:10 +0100

    feat(oms): separate invoice draft creation from line billing

--- app/Http/Controllers/Modules/oms/SimplifiedOrderNoteController.php
+++ app/Http/Controllers/Modules/oms/SimplifiedOrderNoteController.php
@@ -68,6 +68,7 @@ class SimplifiedOrderNoteController extends Controller
             'documentScope' => $documentScope,
             'orderNote' => $orderNote,
             'currencyMeta' => $currencyMeta,
+            'workingInvoiceId' => (int) $request->integer('working_invoice_id'),
             'draftInvoices' => $orderNote
                 ? $this->invoiceWorkflow->getDraftInvoicesForSupplier((int) $orderNote->supplier_id)
                 : collect(),

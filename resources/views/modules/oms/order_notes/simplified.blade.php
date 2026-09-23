565022ada495511173c07be0a029a53341630abe
Bruno Fernandes <brunofernandes@all-stars-distribution.com>
2026-09-23 15:48:33 +0100

    fix(oms): remove empty supplier alert

--- resources/views/modules/oms/order_notes/simplified.blade.php
+++ resources/views/modules/oms/order_notes/simplified.blade.php
@@ -175,8 +175,6 @@
             @endforeach
 </div>
 @endif
-@else
-@if($selectedSupplierId)<div class="alert alert-info">This supplier does not have any orders yet.</div>@endif
 @endif
 </div>
 <div class="modal fade" id="backordersModal" tabindex="-1"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">PS9 backorders</h5><button type="button" class="btn-close" data-bs-dismiss="modal"></button></div><table class="table mb-0"><tbody id="backordersRows"></tbody></table></div></div></div>
